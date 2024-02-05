68617c8bba74b8fc65c590004fb51370ef18c182
dima5513 <super.dimka5513@yandex.ru>
2024-02-05 12:40:36 +0300

    change connection database url and render notification when user is not auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,6 @@ import { useSession } from "next-auth/react";
 export default function Home() {
 	const session = useSession();
 
-	return <div>{session.data && <Profile />}</div>;
+	return <div>{session.data ? <Profile /> : <h1>Войдите</h1>}</div>;
 }
 
